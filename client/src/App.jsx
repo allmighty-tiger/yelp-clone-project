ce95761be2e555d57db79d7c9202ff79f658a3dd
allmighty-tiger <nastia.smolkina@gmail.com>
2023-07-07 09:45:33 -0400

    feat: add components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Updatepage from './routes/Updatepage'
+import RestaurantdetailPage from './routes/RestaurantdetailPage'
+import Home from './routes/Home'
 
 const App = () => {
-    return <div>App</div>
+    return <div>
+    <Router>
+        <Routes>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/restaurants/:id/update" component={Updatepage}/>
+            <Route exact path="/restaurants/:id" component={RestaurantdetailPage}/>
+       </Routes>
+    </Router>
+    </div>
 }
 
 export default App
---
+++ client/src/routes/Updatepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Updatepage = () => {
+  return (
+    <div>Updatepage</div>
+  )
+}
+
+export default Updatepage
